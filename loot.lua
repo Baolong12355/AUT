f2095977aaca057e5d448918b1af3731c21efa21
Baolong12355 <125375091+Baolong12355@users.noreply.github.com>
2025-08-24 09:59:07 +0700

    Update loot.lua

--- loot.lua
+++ loot.lua
@@ -63,7 +63,7 @@ end
 
 spawn(function()
     while true do
-        task.wait(0.1)
+        task.wait(10)
         if _G.LootEnabled and not _G.LootCollecting then
             for _, chest in ipairs(workspace:GetDescendants()) do
                 if isValidChest(chest) then
